8363dcce9c7638de64108a8fc19b07eb03138bfb
Benjamin Worthington <benworthin@icloud.com>
2021-03-27 23:36:13 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/pf2_lab10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pf2_lab10.dir/main.cpp.o"
+  "pf2_lab10"
+  "pf2_lab10.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pf2_lab10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
